836ee635472a1d422a33f8f3edb1c9c97f175c0a
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-08-14 16:11:11 -0400

    Update menu.h

--- menu.h
+++ menu.h
@@ -5,25 +5,18 @@
 #include "external/imgui/backends/imgui_impl_win32.h"
 #include "external/imgui/backends/imgui_impl_opengl2.h"
 
-
-namespace Menu
-{
-
-}
-
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam); // TODO: shouldn't be extern in cpp file? move to .h file
 
-class Menuu
+class Menu
 {
 public:
 	Menuu() {};
 	void toggleMenu();
 	void render();
 	void init();
-	void startRender(); // TODO: move into render
 	void render();
-	void endRender(); // TODO: move into render
 	BOOL __stdcall newSwapBuffers(HDC hdc); 
 private:
 
 };
+
