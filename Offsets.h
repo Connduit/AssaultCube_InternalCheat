daaba33a2db48ac91e5c505cc248f1567a64c5d3
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-08-15 10:14:42 -0400

    Update Offsets.h

--- Offsets.h
+++ Offsets.h
@@ -1,6 +1,19 @@
 #pragma once
 
+// TODO:
 namespace Offsets
 {
+    struct Player
+    {
+        /*
+        Player() : 
+            var1(0),
+            var2()
+        {}
+        int var1;
+        int var2;
+        */
+        static constexpr uintptr_t localPlayer = 0x0017E0A8;
+    };
 
 }
